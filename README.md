f6348cfb9c8e4dbec0699766c0f087f922fe494a
Danielle Hoo <daniellem.hoo@gmail.com>
2020-04-29 18:36:44 -0400

    creating title array from jsob object

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+## A Data Visualization of NYTimes Best Seller Titles
+
 ## Helper Code
-* 1. https://stackoverflow.com/questions/55555095/how-to-add-commas-in-between-json-objects-present-in-a-txt-file-and-then-conver
+
+* https://stackoverflow.com/questions/55555095/how-to-add-commas-in-between-json-objects-present-in-a-txt-file-and-then-conver
+* https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
--- main.js
+++ main.js
@@ -1,21 +1,58 @@
-// function init(){
-//     getData(function(response) {
-//         let parsed_JSON = JSON.parse(response)
-//     })
-// }
+function init () {
+  getData(function (response) {
+    let parsed_JSON = JSON.parse(response)
+    getTitles(parsed_JSON)
+  })
+}
 
-// function getData (callback) {
-//   let data = new XMLHttpRequest()
-//   data.overrideMimeType('application/json')
-//   data.open('GET', 'nyt2.json', true)
-//   data.onreadystatechange = function () {
-//     if (data.readyState == 4 && data.status == '200') {
-//       // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-//       callback(data.responseText)
-//       console.log(data.response)
-//     }
-//   }
-//   data.send(null)
-// }
+function getData (callback) {
+  let data = new XMLHttpRequest()
+  data.overrideMimeType('application/json')
+  data.open('GET', 'nyTimesData.json', true)
+  data.onreadystatechange = function () {
+    if (data.readyState == 4 && data.status == '200') {
+      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+      callback(data.responseText)
+    }
+  }
+  data.send(null)
+}
 
-// init()
+// get all titles from JSON object
+// push titles to new array and filter out duplicates
+// convert each individual word to a string and push it to a new array
+function getTitles (parsed_JSON) {
+  let globalData = parsed_JSON
+  let titleArray = []
+  parsed_JSON.map((item, index) => {
+    titleArray.indexOf(item.title) == -1 ? titleArray.push(item.title) : null
+  })
+
+//   console.log(titleArray)
+
+//   const map = titleArray.reduce(
+//     (acc, e) => acc.set(e, (acc.get(e) || 0) + 1),
+//     new Map()
+//   )
+//   console.info([...map.keys()])
+//   console.info([...map.values()])
+//   console.info([...map.entries()])
+
+
+const counts = titleArray.reduce(
+  (acc, value) => ({
+    ...acc,
+    [value]: (acc[value] || 0) + 1
+  }),
+  {}
+)
+
+
+console.log(counts)
+
+}
+
+init()
+
+// to do:
+// separate each word in the title into a string and push that to a new array
