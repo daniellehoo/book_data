cb19b2f89d7c770ac02a5cc4eb711ef9070a2318
Danielle Hoo <daniellem.hoo@gmail.com>
2020-04-30 18:34:44 -0400

    restructure data

--- main.js
+++ main.js
@@ -27,12 +27,13 @@ function getTitles (parsed_JSON) {
   parsed_JSON.map((item, index) => {
     titleArray.indexOf(item.title) == -1 ? titleArray.push(item.title) : null
   })
-  let splitText = titleArray.join(" ").split(" ")
-
-//   splitText.map(())
-//   splitText.indexOf()
-
-  console.log(splitText)
+  let splitTextArr = titleArray.join(" ").split(" ")
+  splitTextArr.map((word, index) => {
+    let removePunctuation = word.replace(/[^a-zA-Z0-9']/g, '')
+    uniqueWords.push(removePunctuation)
+  })
+  console.log(uniqueWords)
+  
 
 //   const map = splitText.reduce(
 //     (acc, e) => acc.set(e, (acc.get(e) || 0) + 1),
@@ -43,7 +44,7 @@ function getTitles (parsed_JSON) {
 //   console.info([...map.entries()])
 
 
-const counts = splitText.reduce(
+const counts = uniqueWords.reduce(
   (acc, value) => ({
     ...acc,
     [value]: (acc[value] || 0) + 1
@@ -51,9 +52,104 @@ const counts = splitText.reduce(
   {}
 )
 
-
 console.log(counts)
 
+let jsonData = JSON.stringify(counts)
+
+let newNewArray = []
+// counts.map(item => {
+// let newItem = item.JSON.stringify(item)
+// newItem.push(newNewArray)
+// })
+
+
+// console.log(newNewArray)
+
+
+
+// var diameter = 960,
+//   format = d3.format(',d'),
+//   color = d3.scaleOrdinal(d3.schemeCategory20c)
+
+// var bubble = d3
+//   .pack()
+//   .size([diameter, diameter])
+//   .padding(1.5)
+
+// var svg = d3
+//   .select('body')
+//   .append('svg')
+//   .attr('width', diameter)
+//   .attr('height', diameter)
+//   .attr('class', 'bubble')
+
+// d3.json(jsonData, function (data) {
+//   // if (error) throw error
+
+//   var root = d3
+//     .hierarchy(classes(data))
+//     .sum(function (d) {
+//       return d.value
+//     })
+//     .sort(function (a, b) {
+//       return b.value - a.value
+//     })
+
+//   bubble(root)
+//   var node = svg
+//     .selectAll('.node')
+//     .data(root.children)
+//     .enter()
+//     .append('g')
+//     .attr('class', 'node')
+//     .attr('transform', function (d) {
+//       return 'translate(' + d.x + ',' + d.y + ')'
+//     })
+
+//   node.append('title').text(function (d) {
+//     return d.data.className + ': ' + format(d.value)
+//   })
+
+//   node
+//     .append('circle')
+//     .attr('r', function (d) {
+//       return d.r
+//     })
+//     .style('fill', function (d) {
+//       return color(d.data.packageName)
+//     })
+
+//   node
+//     .append('text')
+//     .attr('dy', '.3em')
+//     .style('text-anchor', 'middle')
+//     .text(function (d) {
+//       return d.data.className.substring(0, d.r / 3)
+//     })
+// })
+
+// // Returns a flattened hierarchy containing all leaf nodes under the root.
+// function classes (root) {
+//   var classes = []
+
+//   function recurse (name, node) {
+//     if (node.children)
+//       node.children.forEach(function (child) {
+//         recurse(node.name, child)
+//       })
+//     else
+//       classes.push({
+//         packageName: name,
+//         className: node.name,
+//         value: node.size
+//       })
+//   }
+
+//   recurse(null, root)
+//   return { children: classes }
+// }
+
+// d3.select(self.frameElement).style('height', diameter + 'px')
 }
 
 init()
